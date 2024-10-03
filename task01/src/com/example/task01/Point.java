d3452ddad43145ceca5def744952890b08e83f13
iDIOtu <95068240+iDIOtu@users.noreply.github.com>
2024-10-03 21:19:13 +0500

    Голубев Никита. Задание 01

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -1,11 +1,34 @@
 package com.example.task01;
 
-/**
- * Класс точки на плоскости
- */
-public class Point {
+public class Point
+{
     int x;
     int y;
+
+    public Point(){}
+
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void flip()
+    {
+        int temp = -x;
+        x = -y;
+        y = temp;
+    }
+
+    double distance(Point point)
+    {
+        return Math.sqrt(Math.pow(point.x - x, 2) + Math.pow(point.y - y, 2));
+    }
+
+    public String toString()
+    {
+        return "(" + x + ", " + y + ")";
+    }
 
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
