f127461699c69497ad866e891abc0a01d8d7f29c
iDIOtu <95068240+iDIOtu@users.noreply.github.com>
2024-10-03 23:24:18 +0500

    Голубев Никита. Задание 04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,7 +1,19 @@
 package com.example.task04;
 
-public class Task04Main {
-    public static void main(String[] args) {
+public class Task04Main
+{
+    public static void main(String[] args)
+    {
+        Point point1 = new Point(0, 0);
+        Point point2 = new Point(4, 2);
+        Line line = new Line(point1, point2);
 
+        System.out.println(line);
+
+        Point testPoint1 = new Point(2, 1); // На линии
+        Point testPoint2 = new Point(1, 0); // Не на линии
+
+        System.out.println("Is point collinear? " + line.isCollinearLine(testPoint1)); // true
+        System.out.println("Is point collinear? " + line.isCollinearLine(testPoint2)); // false
     }
 }
