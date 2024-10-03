7da4ac0405c902c4f022c7f9944e822711dbf9c3
iDIOtu <95068240+iDIOtu@users.noreply.github.com>
2024-10-03 22:46:10 +0500

    Голубев Никита. Задание 03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,15 @@
 package com.example.task03;
 
 public class Task03Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        Complex number1 = new Complex(3, 4);
+        Complex number2 = new Complex(1, 2);
 
+        Complex sum = number1.add(number2);
+        System.out.println("Sum: " + sum);
+
+        Complex product = number1.multiply(number2);
+        System.out.println("Product: " + product);
     }
 }
